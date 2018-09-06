abd99574837222aa83523767c53f8e0e57971eff
Cyberarm <matthewlikesrobots@gmail.com>
2018-09-06 08:05:37 -0500

    Non-blocking morse code, probably.

--- morse_code.ino
+++ morse_code.ino
@@ -1,6 +1,10 @@
 char morseCodeString[] = "HELLO WORLD ";
 boolean morseCodeDie = false;
 
+int morseIndex, morseListIndex = 0;
+int lastMorseListIndex = -1;
+long morseUpdateMs = 0;
+
 int DOT = 100; // ms
 int DASH= DOT*3;
 int SHORT=DOT*5;
@@ -46,36 +50,74 @@ int morseCode_0[] = {DASH, DASH, DASH, DASH, DASH};
 
 void MODE_morsecode() {
 
-  for(int i = 0; i < sizeof(morseCodeString); i++) {
-    if (morseCodeString[i] == ' ') {
-      // color off
-      sleep(MEDIUM);
-      continue;
-    }
+  // for(int i = 0; i < sizeof(morseCodeString); i++) {
+  //   if (morseCodeString[i] == ' ') {
+  //     // color off
+  //     sleep(MEDIUM);
+  //     continue;
+  //   }
 
-    int *list = getMorseCodeForChar(morseCodeString[i]);
+  //   int *list = getMorseCodeForChar(morseCodeString[i]);
 
 
-    for(int j = 0; j < sizeof(list); j++) {
-      // color on
-      sleep(list[j]);
-      // color off
-      sleep(DOT);
-    }
+  //   for(int j = 0; j < sizeof(list); j++) {
+  //     // color on
+  //     sleep(list[j]);
+  //     // color off
+  //     sleep(DOT);
+  //   }
 
-    // color off
-    sleep(DASH);
+  //   // color off
+  //   sleep(DASH);
+  // }
+  if (millis() > morseUpdateMs) {
+    animateMorseCode();
   }
-
 }
 
-void sleep(long ms) {
-  handleButton(buttonPin);
-  delay(ms);
-  if (true) {} // detect if mode changed
-  morseCodeDie = false;
-  resetButtonData();
+void animateMorseCode() {
+  if (morseIndex < sizeof(morseCodeString)) {
+    if (morseCodeString[morseIndex] == ' ') {
+      resetLeds();
+      morseUpdateMs = (millis()+MEDIUM);
+      morseIndex++;
+    } else {
+
+      int *list = getMorseCodeForChar(morseCodeString[morseIndex]);
+      if (morseListIndex < sizeof(list)) {
+        if (lastMorseListIndex == morseListIndex) {
+          resetLeds();
+          morseUpdateMs = (millis()+DOT);
+          morseListIndex++;
+        } else {
+          morseLedsOn();
+          morseUpdateMs = (millis() + list[morseListIndex]);
+          lastMorseListIndex = morseListIndex;
+        }
+      } else {
+        morseListIndex = 0;
+        lastMorseListIndex = -1;
+      }
+    }
+
+  } else {
+    morseIndex = 0;
+  }
 }
+
+void morseLedsOn() {
+  setColor(redPin, maxBrightness);
+  setColor(greenPin, maxBrightness);
+  setColor(bluePin, maxBrightness);
+}
+
+// void sleep(long ms) {
+//   handleButton(buttonPin);
+  // morseCodeString[morseIndex];//   delay(ms);
+//   if (true) {} // detect if mode changed
+//   morseCodeDie = false;
+//   resetButtonData();
+// }
 
 int* getMorseCodeForChar(char str) {
   switch(str) {
