15d309441537d1f0ad279d746ae0374bedb6566d
Cyberarm <matthewlikesrobots@gmail.com>
2018-09-10 12:37:41 -0500

    Optimize global memory usage: ~176 bytes free to ~416 bytes!

--- morse_code.ino
+++ morse_code.ino
@@ -10,42 +10,6 @@ int DASH= DOT*3;
 int SHORT=DOT*5;
 int MEDIUM=DOT*7;
 
-int morseCode_A[] = {DOT, DASH};
-int morseCode_B[] = {DASH, DOT, DOT, DOT};
-int morseCode_C[] = {DASH, DOT, DASH, DOT};
-int morseCode_D[] = {DASH, DOT, DOT};
-int morseCode_E[] = {DOT};
-int morseCode_F[] = {DOT, DOT, DASH, DOT};
-int morseCode_G[] = {DASH, DASH, DOT};
-int morseCode_H[] = {DOT, DOT, DOT, DOT};
-int morseCode_I[] = {DOT, DOT};
-int morseCode_J[] = {DOT, DASH, DASH, DASH};
-int morseCode_K[] = {DASH, DOT, DASH};
-int morseCode_L[] = {DOT, DASH, DOT, DOT};
-int morseCode_M[] = {DASH, DASH};
-int morseCode_N[] = {DASH, DOT};
-int morseCode_O[] = {DASH, DASH, DASH};
-int morseCode_P[] = {DOT, DASH, DASH, DOT};
-int morseCode_Q[] = {DASH, DASH, DOT, DASH};
-int morseCode_R[] = {DOT, DASH, DOT};
-int morseCode_S[] = {DOT, DOT, DOT};
-int morseCode_T[] = {DASH};
-int morseCode_U[] = {DOT, DOT, DASH};
-int morseCode_V[] = {DOT, DOT, DOT, DASH};
-int morseCode_W[] = {DOT, DASH, DASH};
-int morseCode_X[] = {DASH, DOT, DOT, DASH};
-int morseCode_Y[] = {DASH, DOT, DASH, DASH};
-int morseCode_Z[] = {DASH, DASH, DOT, DOT};
-
-int morseCode_1[] = {DOT, DASH, DASH, DASH, DASH};
-int morseCode_2[] = {DOT, DOT, DASH, DASH, DASH};
-int morseCode_3[] = {DOT, DOT, DOT, DASH, DASH};
-int morseCode_4[] = {DOT, DOT, DOT, DOT, DASH};
-int morseCode_5[] = {DOT, DOT, DOT, DOT, DOT};
-int morseCode_6[] = {DASH, DOT, DOT, DOT, DOT};
-int morseCode_7[] = {DASH, DASH, DOT, DOT, DOT};
-int morseCode_8[] = {DASH, DASH, DASH, DOT, DOT};
-int morseCode_9[] = {DASH, DASH, DASH, DASH, DOT};
 int morseCode_0[] = {DASH, DASH, DASH, DASH, DASH};
 
 void MODE_morsecode() {
@@ -120,148 +84,188 @@ int morseSize(char *list)
 
 int *getMorseCodeForChar(char str) {
   switch(str) {
-    case 'A':
-      activeArraySize = (sizeof(morseCode_A)/sizeof(morseCode_A[0]));
-      return morseCode_A;
+    case 'A': {
+      activeArraySize = 2;
+      int code[] = {DOT, DASH};
+      return code;
       break;
-    case 'B':
-      activeArraySize = (sizeof(morseCode_B) / sizeof(morseCode_B[0]));
-      return morseCode_B;
-    case 'C':
-      activeArraySize = (sizeof(morseCode_C) / sizeof(morseCode_C[0]));
-      return morseCode_C;
+    }
+    case 'B':{
+      activeArraySize = 4;
+      int code[] = {DASH, DOT, DOT, DOT};
+      return code;
+    }
+    case 'C':{
+      activeArraySize = 4;
+      int code[] = {DASH, DOT, DASH, DOT};
+      return code;
+    }
+    case 'D':{
+      activeArraySize = 3;
+      int code[] = {DASH, DOT, DOT};
       break;
-    case 'D':
-      activeArraySize = (sizeof(morseCode_D) / sizeof(morseCode_D[0]));
-      return morseCode_D;
+    }
+    case 'E':{
+      activeArraySize = 1;
+      int code[] = {DOT};
       break;
-    case 'E':
-      activeArraySize = (sizeof(morseCode_E) / sizeof(morseCode_E[0]));
-      return morseCode_E;
+    }
+    case 'F':{
+      activeArraySize = 4;
+      int code[] = {DOT, DOT, DASH, DOT};
       break;
-    case 'F':
-      activeArraySize = (sizeof(morseCode_F) / sizeof(morseCode_F[0]));
-      return morseCode_F;
+    }
+    case 'G':{
+      activeArraySize = 3;
+      int code[] = {DASH, DASH, DOT};
       break;
-    case 'G':
-      activeArraySize = (sizeof(morseCode_G) / sizeof(morseCode_G[0]));
-      return morseCode_G;
+    }
+    case 'H':{
+      activeArraySize = 4;
+      int code[] = {DOT, DOT, DOT, DOT};
       break;
-    case 'H':
-      activeArraySize = (sizeof(morseCode_H) / sizeof(morseCode_H[0]));
-      return morseCode_H;
+    }
+    case 'I':{
+      activeArraySize = 2;
+      int code[] = {DOT, DOT};
       break;
-    case 'I':
-      activeArraySize = (sizeof(morseCode_I) / sizeof(morseCode_I[0]));
-      return morseCode_I;
+    }
+    case 'J':{
+      activeArraySize = 4;
+      int code[] = {DOT, DASH, DASH, DASH};
       break;
-    case 'J':
-      activeArraySize = (sizeof(morseCode_J) / sizeof(morseCode_J[0]));
-      return morseCode_J;
+    }
+    case 'K':{
+      activeArraySize = 3;
+      int code[] = {DASH, DOT, DASH};
       break;
-    case 'K':
-      activeArraySize = (sizeof(morseCode_K) / sizeof(morseCode_K[0]));
-      return morseCode_K;
+    }
+    case 'L':{
+      activeArraySize = 4;
+      int code[] = {DOT, DASH, DOT, DOT};
       break;
-    case 'L':
-      activeArraySize = (sizeof(morseCode_L) / sizeof(morseCode_L[0]));
-      return morseCode_L;
-    case 'M':
-      activeArraySize = (sizeof(morseCode_M) / sizeof(morseCode_M[0]));
-      return morseCode_M;
+    }
+    case 'M':{
+      activeArraySize = 2;
+      int code[] = {DASH, DASH};
       break;
-    case 'N':
-      activeArraySize = (sizeof(morseCode_N) / sizeof(morseCode_N[0]));
-      return morseCode_N;
+    }
+    case 'N':{
+      activeArraySize = 2;
+      int code[] = {DASH, DOT};
       break;
-    case 'O':
-      activeArraySize = (sizeof(morseCode_O) / sizeof(morseCode_O[0]));
-      return morseCode_O;
-    case 'P':
-      activeArraySize = (sizeof(morseCode_P) / sizeof(morseCode_P[0]));
-      return morseCode_P;
+    }
+    case 'O':{
+      activeArraySize = 3;
+      int code[] = {DASH, DASH, DASH};
       break;
-    case 'Q':
-      activeArraySize = (sizeof(morseCode_Q) / sizeof(morseCode_Q[0]));
-      return morseCode_Q;
+    }
+    case 'P':{
+      activeArraySize = 4;
+      int code[] = {DOT, DASH, DASH, DOT};
       break;
-    case 'R':
-      activeArraySize = (sizeof(morseCode_R) / sizeof(morseCode_R[0]));
-      return morseCode_R;
+    }
+    case 'Q':{
+      activeArraySize = 4;
+      int code[] = {DASH, DASH, DOT, DASH};
       break;
-    case 'S':
-      activeArraySize = (sizeof(morseCode_S) / sizeof(morseCode_S[0]));
-      return morseCode_S;
+    }
+    case 'R':{
+      activeArraySize = 3;
+      int code[] = {DOT, DASH, DOT};
       break;
-    case 'T':
-      activeArraySize = (sizeof(morseCode_T) / sizeof(morseCode_T[0]));
-      return morseCode_T;
+    }
+    case 'S':{
+      activeArraySize = 3;
+      int code[] = {DOT, DOT, DOT};
       break;
-    case 'U':
-      activeArraySize = (sizeof(morseCode_U) / sizeof(morseCode_U[0]));
-      return morseCode_U;
+    }
+    case 'T':{
+      activeArraySize = 1;
+      int code[] = {DASH};
       break;
-    case 'V':
-      activeArraySize = (sizeof(morseCode_V) / sizeof(morseCode_V[0]));
-      return morseCode_V;
+    }
+    case 'U':{
+      activeArraySize = 3;
+      int code[] = {DOT, DOT, DASH};
       break;
-    case 'W':
-      activeArraySize = (sizeof(morseCode_W) / sizeof(morseCode_W[0]));
-      return morseCode_W;
+    }
+    case 'V':{
+      activeArraySize = 4;
+      int code[] = {DOT, DOT, DOT, DASH};
       break;
-    case 'X':
-      activeArraySize = (sizeof(morseCode_X) / sizeof(morseCode_X[0]));
-      return morseCode_X;
+    }
+    case 'W':{
+      activeArraySize = 3;
+      int code[] = {DOT, DASH, DASH};
       break;
-    case 'Y':
-      activeArraySize = (sizeof(morseCode_Y) / sizeof(morseCode_Y[0]));
-      return morseCode_Y;
+    }
+    case 'X':{
+      activeArraySize = 4;
+      int code[] = {DASH, DOT, DOT, DASH};
       break;
-    case 'Z':
-      activeArraySize = (sizeof(morseCode_Z) / sizeof(morseCode_Z[0]));
-      return morseCode_Z;
+    }
+    case 'Y':{
+      activeArraySize = 4;
+      int code[] = {DASH, DOT, DASH, DASH};
       break;
+    }
+    case 'Z':{
+      activeArraySize = 4;
+      int code[] = {DASH, DASH, DOT, DOT};
+      break;
+    }
 
-    case '1':
-      activeArraySize = (sizeof(morseCode_1) / sizeof(morseCode_1[0]));
-      return morseCode_1;
+    case '1':{
+      activeArraySize = 5;
+      int code[] = {DOT, DASH, DASH, DASH, DASH};
       break;
-    case '2':
-      activeArraySize = (sizeof(morseCode_2) / sizeof(morseCode_2[0]));
-      return morseCode_2;
+    }
+    case '2':{
+      activeArraySize = 5;
+      int code[] = {DOT, DOT, DASH, DASH, DASH};
       break;
-    case '3':
-      activeArraySize = (sizeof(morseCode_3) / sizeof(morseCode_3[0]));
-      return morseCode_3;
+    }
+    case '3':{
+      activeArraySize = 5;
+      int code[] = {DOT, DOT, DOT, DASH, DASH};
       break;
-    case '4':
-      activeArraySize = (sizeof(morseCode_4) / sizeof(morseCode_4[0]));
-      return morseCode_4;
+    }
+    case '4':{
+      activeArraySize = 5;
+      int code[] = {DOT, DOT, DOT, DOT, DASH};
       break;
-    case '5':
-      activeArraySize = (sizeof(morseCode_5) / sizeof(morseCode_5[0]));
-      return morseCode_5;
+    }
+    case '5':{
+      activeArraySize = 5;
+      int code[] = {DOT, DOT, DOT, DOT, DOT};
       break;
-    case '6':
-      activeArraySize = (sizeof(morseCode_6) / sizeof(morseCode_6[0]));
-      return morseCode_6;
+    }
+    case '6':{
+      activeArraySize = 5;
+      int code[] = {DASH, DOT, DOT, DOT, DOT};
       break;
-    case '7':
-      activeArraySize = (sizeof(morseCode_7) / sizeof(morseCode_7[0]));
-      return morseCode_7;
+    }
+    case '7':{
+      activeArraySize = 5;
+      int code[] = {DASH, DASH, DOT, DOT, DOT};
       break;
-    case '8':
-      activeArraySize = (sizeof(morseCode_8) / sizeof(morseCode_8[0]));
-      return morseCode_8;
+    }
+    case '8':{
+      activeArraySize = 5;
+      int code[] = {DASH, DASH, DASH, DOT, DOT};
       break;
-    case '9':
-      activeArraySize = (sizeof(morseCode_9) / sizeof(morseCode_9[0]));
-      return morseCode_9;
+    }
+    case '9':{
+      activeArraySize = 5;
+      int code[] = {DASH, DASH, DASH, DASH, DOT};
       break;
-    case '0':
-      activeArraySize = (sizeof(morseCode_0) / sizeof(morseCode_0[0]));
-      return morseCode_0;
+    }
+    case '0':{
+      activeArraySize = 5;
+      int code[] = {DASH, DASH, DASH, DASH, DASH};
       break;
+    }
 
     default:
       activeArraySize = 0;
